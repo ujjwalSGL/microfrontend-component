46b2ae8b95b8e8c4f9b766316a79d61c13452459
ujjwalSGL <ujjwal.tyagi@shipglobal.in>
2025-05-02 10:37:25 +0530

    netlify.toml

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -1,3 +1,45 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import federation from "@originjs/vite-plugin-federation";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     federation({
+//       name: "component",
+//       filename: "components.js",
+//       exposes: {
+//         "./Button": "./src/components/ui/button.tsx",
+//       },
+//       shared: ["react", "react-dom"],
+//     }),
+//   ],
+//   build: {
+//     modulePreload: false,
+//     target: "esnext",
+//     minify: false,
+//     cssCodeSplit: false,
+//   },
+//   server: {
+//     port: 5001,
+//     strictPort: true,
+//     cors: true,
+//   },
+//   preview: {
+//     port: 5001,
+//     strictPort: true,
+//     cors: true,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
@@ -26,10 +68,20 @@ export default defineConfig({
   server: {
     port: 5001,
     strictPort: true,
+    cors: {
+      origin: "*",
+      methods: ["GET", "OPTIONS"],
+      allowedHeaders: ["X-Requested-With", "Content-Type", "Accept"],
+    },
   },
   preview: {
     port: 5001,
     strictPort: true,
+    cors: {
+      origin: "*",
+      methods: ["GET", "OPTIONS"],
+      allowedHeaders: ["X-Requested-With", "Content-Type", "Accept"],
+    },
   },
   resolve: {
     alias: {
